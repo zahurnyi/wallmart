d43508496cd91c390a7fa9613c5150cd2c095f5e
Dmytro <studentdima21@gmail.com>
2023-02-03 18:38:54 +0200

    beta 2.2

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-// const btnAdd = document.querySelector('.buttonAdd')
 const btnAdd = document.createElement('button')
 const btnCount = document.createElement('button')
 const btnClear = document.createElement('button')
 const buttons = document.createElement('div')
 const wrapper = document.querySelector('.wrapper')
 const basket = document.querySelector('.basket')
+const productsOpen = document.querySelector('.products')
+const productsClose = document.createElement('button');
+const productsAdd = document.createElement('button');
+const items = document.querySelectorAll('.products__item');
+const calculating = document.querySelector('.calculate');
+const closeCalculateBtn = document.createElement('button')
 
-document.querySelector('body').append(wrapper)
 
 wrapper.append(buttons)
 buttons.append(btnAdd, btnCount, btnClear)
+productsOpen.append(productsClose, productsAdd);
+document.querySelector('body').append(wrapper)
 
 btnAdd.classList.add('buttonAdd')
 btnCount.classList.add('buttonCount')
 btnClear.classList.add('buttonClear')
 buttons.classList.add('buttons')
 wrapper.classList.add('.wrapper')
+productsAdd.classList.add('closeProducts');
+productsClose.classList.add('close');
 
 btnAdd.textContent = 'choose products'
 btnCount.textContent = 'change counting'
 btnClear.textContent = 'clear basket'
+productsClose.textContent = 'close';
+productsAdd.textContent = 'add';
+closeCalculateBtn.textContent = 'close'
 
-
-const productsOpen = document.querySelector('.products')
+items.forEach(item => {
+	const itemAdd = () => {
+		item.classList.toggle('products__active')
+	}
+	item.addEventListener('click', itemAdd)
+})
 
 const openProducts = () => {
 	productsOpen.style.display = 'block'
@@ -32,20 +47,6 @@ const clearProducts = () => {
 	basket.textContent = ''
 }
 
-btnClear.addEventListener('click', clearProducts)
-btnAdd.addEventListener('click', openProducts)
-
-const productsClose = document.createElement('button');
-const productsAdd = document.createElement('button');
-
-productsClose.textContent = 'close';
-productsAdd.textContent = 'add';
-
-productsAdd.classList.add('closeProducts');
-productsClose.classList.add('close');
-
-productsOpen.append(productsClose, productsAdd);
-
 const closeMenu = () => {
 	productsOpen.style.display = 'none'
 	items.forEach(item => {
@@ -53,48 +54,36 @@ const closeMenu = () => {
 	})
 }
 
-productsClose.addEventListener('click', closeMenu)
-
-productsAdd.addEventListener('click', () => {
+const addingProducts = () => {
 	items.forEach(item => {
-		if (item.classList.contains('products__active')) {
-			const basket = document.querySelector('.basket')
-			const clone = item.cloneNode(true)
-			basket.append(clone)
-			productsOpen.style.display = 'none'
-			item.classList.remove('products__active')
-		} else {
-			productsClose.addEventListener('click', closeMenu)
-		}
-	})
+	if (item.classList.contains('products__active')) {
+		const basket = document.querySelector('.basket')
+		const clone = item.cloneNode(true)
+		clone.style.width = "100%"
+		basket.append(clone)
+		productsOpen.style.display = 'none'
+		item.classList.remove('products__active')
+	} else {
+		productsClose.addEventListener('click', closeMenu)
+	}
 })
-
-// const btnCount = document.querySelector('.buttonCount')
-const calculating = document.querySelector('.calculate')
-console.log(btnCount);
+}
 
 const openCalculate = () => {
 	calculating.style.display = 'block';
+	calculating.append(closeCalculateBtn)
+	openCalculate.append(closeCalculateBtn)
 }
-btnCount.addEventListener('click', openCalculate);
-
-const calculateClose = document.createElement('button');
-calculateClose.textContent = 'close'
-calculateClose.classList.add('close')
-calculating.append(calculateClose)
 
 const closeCalculate = () => {
 	calculating.style.display = 'none'
 }
-calculateClose.addEventListener('click', closeCalculate)
 
-const items = document.querySelectorAll('.products__item');
-console.log(items);
+productsAdd.addEventListener('click', addingProducts)
+btnCount.addEventListener('click', openCalculate);
+btnClear.addEventListener('click', clearProducts)
+closeCalculateBtn.addEventListener('click', closeCalculate)
+btnAdd.addEventListener('click', openProducts)
+productsClose.addEventListener('click', closeMenu)
 
-items.forEach(item => {
-	const itemAdd = () => {
-		item.classList.toggle('products__active')
-	}
-	item.addEventListener('click', itemAdd)
-})
 
